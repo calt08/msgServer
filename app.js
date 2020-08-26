cb49ab38156d9f871f02f5adb6ee47d5133c9209
calt08 <cesarlopez2808@gmail.com>
2020-08-26 12:26:15 -0400

    auto-increment id, PUT and DELETE done

--- app.js
+++ app.js
@@ -2,25 +2,32 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-msgs = [
-    { id: 1, msg: "Hello" },
-    { id: 2, msg: "World" },
-];
+num = 0;
+msgs = [];
+
+msgs.push({ id: ++num, msg: "Hello" });
+msgs.push({ id: ++num, msg: "World" });
+
 
 app.get("/messages", function (req, res) {
     res.status(200).send(msgs);
 });
 
 app.post("/messages", function (req, res) {
-    // console.log(req.body.id);
-    msgs.push({ id: req.body.id, msg: req.body.msg });
-    res.status(200).send(req.body);
+    message = { id: ++num, msg: req.body.msg };
+    msgs.push(message);
+    res.status(200).send(message);
 });
 
 app.put("/messages/:id", function (req, res) {
-    // console.log(req.params.id);
-    message = msgs.find((elem) => elem === req.params.id);
+    message = msgs.find((elem) => elem.id == req.params.id);
     message.msg = req.body.msg;
+    res.status(200).send(message);
+});
+
+app.delete("/messages/:id", function (req, res) {
+    message = msgs.find((elem) => elem.id == req.params.id);
+    msgs.splice(msgs.indexOf(message), 1);
     res.status(200).send(message);
 });
 
